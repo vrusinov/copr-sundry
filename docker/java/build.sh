4361fe39b1fcd98dfb449d66d1f7ab68e4080f0a
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2023-08-17 23:08:19 +0100

    Update java docker

--- docker/java/build.sh
+++ docker/java/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 date_v=$(date '+%Y-%m-%d')
-v="17.0.3_p7"
+v="17.0.6_p10"
 n="java"
 
 docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
--- docker/java/clean.sh
+++ docker/java/clean.sh
@@ -2,14 +2,16 @@
 
 rm -r /usr/lib/python*
 rm -rf /usr/lib64/binutils/x86_64-pc-linux-gnu/*/ldscripts
-rm -rf /usr/lib64/gconv/GB18030.so
-rm -rf /usr/lib64/gconv/IBM1371.so
 rm -rf /usr/lib64/gconv/IBM1388.so
 rm -rf /usr/lib64/gconv/libGB.so
 rm -rf /usr/lib64/gconv/libISOIR165.so
 rm -rf /usr/lib64/perl5
 rm -rf /usr/share/sgml/
-rm /usr/lib64/gconv/IBM1399.so
+rm /usr/bin/cmake
+rm /usr/bin/cpack
+rm /usr/bin/ctest
+rm /usr/lib64/gconv/GB18030.so
+rm /usr/lib64/gconv/IBM1371.so
 
 # Remove this script:
 rm -rf /usr/local

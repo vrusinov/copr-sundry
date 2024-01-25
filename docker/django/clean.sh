9adff0f0d0f7400e5818c7019407ddb3e46f4085
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2024-01-25 22:38:05 +0000

    Update django to use Python 3.10

--- docker/django/clean.sh
+++ docker/django/clean.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+alias rm='rm -r'
+
 set -e
 
-rm -f /usr/lib64/gconv/BIG5HKSCS.so
-rm -f /usr/lib64/gconv/IBM1399.so
-rm -r /usr/lib/python3.9/site-packages/Cython
-rm -r /usr/lib/python3.9/site-packages/pydantic
-rm -r /usr/lib/python3.9/site-packages/setuptools
-rm -rf /usr/lib64/perl5
-rm -rf /usr/share/sgml/
+rm /usr/lib/gcc/x86_64-pc-linux-gnu/*/libatomic.a
+rm /usr/lib/python*/site-packages/setuptools
+rm /usr/lib64/gconv/GBBIG5.so
+rm /usr/lib64/gconv/libGB.so
+rm /usr/lib64/gconv/libISOIR165.so

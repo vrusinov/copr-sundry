555ae8ef84f49ecdbf59b861500aa748f2beb8dc
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2024-08-16 12:01:52 +0100

    Update java docker

--- docker/java/clean.sh
+++ docker/java/clean.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
+set -e
+
 alias rm='rm -r'
 
 rm /opt/openjdk-bin-*/legal
-rm /usr/bin/ctest
 rm /usr/lib/python*
-rm /usr/lib64/binutils/x86_64-pc-linux-gnu/*/ldscripts
-rm /usr/lib64/gconv/GB18030.so
-rm /usr/lib64/gconv/IBM1388.so
-rm /usr/lib64/gconv/JOHAB.so
-rm /usr/lib64/gconv/libGB.so
-rm /usr/lib64/gconv/libISOIR165.so
-rm /usr/lib64/gconv/SHIFT_JISX0213.so
-rm /usr/lib64/gconv/TSCII.so
-rm /usr/lib64/perl5
-rm /usr/share/sgml/
+rm /usr/lib64/binutils/x86_64-pc-linux-gnu/*/libbfd-*.gentoo-sys-devel-binutils-st.so
+rm /usr/lib64/gconv/CP1258.so
+rm /usr/lib64/gconv/gconv-modules.d/
 
 # Remove this script:
 rm -rf /usr/local

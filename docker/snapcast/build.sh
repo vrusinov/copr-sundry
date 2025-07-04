2468c258dd1165cc4911bb9b89c0c8400545cfb6
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2025-07-04 14:45:06 +0100

    Snapcast docker

---
+++ docker/snapcast/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2025 Vladimir Rusinov <vladimir.rusinov@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -x
+
+date_v=$(date '+%Y-%m-%d')
+v="0.27.0"
+n="snapcast"
+
+docker build -t vrusinov/$n:latest -t vrusinov/$n:$date_v -t vrusinov/$n:$v -t vrusinov/$n:$v.$date_v .
+#docker run --rm -it vrusinov/$n:latest
+docker push vrusinov/$n:latest
+docker push vrusinov/$n:$date_v
+docker push vrusinov/$n:$v
+docker push vrusinov/$n:$v.$date_v
---
+++ docker/snapcast/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2025 Vladimir Rusinov <vladimir.rusinov@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+alias rm='rm -r'
+set -e
+
+rm /usr/bin/git-upload-archive
+rm /usr/bin/scalar
+rm /usr/lib64/gconv

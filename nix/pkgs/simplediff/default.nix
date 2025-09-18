e906c6f8300fdcd4b3c040dcc9b1a092ae6be3b7
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2025-09-18 20:22:06 +0100

    Update simplediff

--- nix/pkgs/simplediff/default.nix
+++ nix/pkgs/simplediff/default.nix
@@ -6,10 +6,10 @@
 
 pkgs.python3Packages.buildPythonApplication rec {
   pname = "simplediff";
-  version = "1.0";
+  version = "1.1";
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-V0doszxnFBkOrwVd3RrobAcj6JxYFcPwkAJkXGRTYLY=";
+    hash = "sha256-2ynoLGTswIPjYwi1hPxImFIlQh+Fy2TYmTKTNK3jK9o=";
   };
 
 }

a05cc7e27675d4f1c6fc2475f220e553787b3326
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2025-12-23 12:57:51 +0000

    Update java11 docker

--- docker/java11/clean.sh
+++ docker/java11/clean.sh
@@ -7,12 +7,12 @@ set -e
 
 alias rm='rm -r'
 
+rm /etc/systemd
 rm /lib/systemd
 rm /opt/openjdk-bin-*/man
-rm /usr/bin/gpg
-rm /usr/bin/openssl
+rm /usr/bin/re2rust
 rm /usr/lib/python*
-rm /usr/lib64/tcl8.6
+rm /usr/lib64/binutils/*/*/*.a
 rm /usr/share/gtk-doc/
 rm /usr/share/libtool
 
